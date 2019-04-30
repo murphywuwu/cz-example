71bcec81ddec1d490c06a726fc626838a04e9111
murphywuwu <murphywuwu@gmail.com>
2019-04-30 10:36:24 +0800

    docs(root): change cz-config
    replace cz-config
    
    ISSUES CLOSED: #32

--- .cz-config.js
+++ .cz-config.js
@@ -3,53 +3,46 @@
 module.exports = {
 
   types: [
-    {value: 'feat',     name: 'feat:     A new feature'},
-    {value: 'fix',      name: 'fix:      A bug fix'},
-    {value: 'docs',     name: 'docs:     Documentation only changes'},
-    {value: 'style',    name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'},
-    {value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature'},
-    {value: 'perf',     name: 'perf:     A code change that improves performance'},
-    {value: 'test',     name: 'test:     Adding missing tests'},
-    {value: 'chore',    name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'},
-    {value: 'revert',   name: 'revert:   Revert to a commit'},
-    {value: 'WIP',      name: 'WIP:      Work in progress'}
+    {
+      value: 'WIP',
+      name : '💪  WIP:      Work in progress'
+    },
+    {
+      value: 'feat',
+      name : '✨  feat:     A new feature'
+    },
+    {
+      value: 'fix',
+      name : '🐞  fix:      A bug fix'
+    },
+    {
+      value: 'refactor',
+      name : '🛠  refactor: A code change that neither fixes a bug nor adds a feature'
+    },
+    {
+      value: 'docs',
+      name : '📚  docs:     Documentation only changes'
+    },
+    {
+      value: 'test',
+      name : '🏁  test:     Add missing tests or correcting existing tests'
+    },
+    {
+      value: 'chore',
+      name : '🗯  chore:    Changes that don\'t modify src or test files. Such as updating build tasks, package manager'
+    },
+    {
+      value: 'style',
+      name : '💅  style:    Code Style, Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'
+    },
+    {
+      value: 'revert',
+      name : '⏪  revert:   Revert to a commit'
+    }
   ],
 
-  scopes: [
-    {name: 'accounts'},
-    {name: 'admin'},
-    {name: 'exampleScope'},
-    {name: 'changeMe'}
-  ],
-
-  // it needs to match the value for field type. Eg.: 'fix'
-  /*
-  scopeOverrides: {
-    fix: [
-      {name: 'merge'},
-      {name: 'style'},
-      {name: 'e2eTest'},
-      {name: 'unitTest'}
-    ]
-  },
-  */
-  // override the messages, defaults are as follows
-  messages: {
-    type: 'Select the type of change that you\'re committing:',
-    scope: '\nDenote the SCOPE of this change (optional):',
-    // used if allowCustomScopes is true
-    customScope: 'Denote the SCOPE of this change:',
-    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
-    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-    breaking: 'List any BREAKING CHANGES (optional):\n',
-    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
-    confirmCommit: 'Are you sure you want to proceed with the commit above?'
-  },
+  scopes: [],
 
   allowCustomScopes: true,
-  allowBreakingChanges: ['feat', 'fix'],
-
-  // limit subject length
-  subjectLimit: 100
-  
+  allowBreakingChanges: ["feat", "fix"]
 };
